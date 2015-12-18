9f89172947745a3883b47cbac2edafb074b39a53
thewisenerd <thewisenerd@protonmail.com>
2015-12-18 08:40:45 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+  name = "aac2",
+  packages = ["src/aac2"],
+  version = "0.0.1",
+  description = "AndroidAutoCompiler2",
+  author = "thewisenerd",
+  author_email = "thewisenerd@protonmail.com",
+  url = "http://thewisenerd.me/",
+  keywords = ["android", "compile", "script"],
+  classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Development Status :: Beta",
+  ],
+  long_description = """\
+AndroidAutoCompiler 2
+"""
+)
