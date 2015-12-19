f9b0325f59d5c5b88c2723994c4c75cb11686b14
thewisenerd <thewisenerd@protonmail.com>
2015-12-19 16:03:50 +0530

    install_deps: install schedtool

--- src/__install_deps.py
+++ src/__install_deps.py
@@ -79,12 +79,13 @@ def __install_deps():
   # + openjdk-7-jre
   #
   # + bc
+  # + schedtool
   deps = [
     "git-core", "gnupg", "flex", "bison", "gperf", "build-essential", "zip",
     "curl", "zlib1g-dev", "gcc-multilib", "g++-multilib", "libc6-dev-i386",
     "lib32ncurses5-dev", "x11proto-core-dev", "libx11-dev", "lib32z1-dev",
     "ccache", "libgl1-mesa-dev", "libxml2-utils", "xsltproc", "unzip",
-    "openjdk-7-jdk", "openjdk-7-jre", "bc"
+    "openjdk-7-jdk", "openjdk-7-jre", "bc", "schedtool"
   ]
 
   for pkg_name in deps:
